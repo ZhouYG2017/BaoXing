d7850e11cfa228c6fc1695e06818f0a769fcc406
Administrator <Administrator@VD9V7NAVEN8SGPJ>
2019-07-12 10:22:37 +0800

    1

---
+++ DoNet.Code/LogExt/DataOperateLogEntity.cs
@@ -0,0 +1,35 @@
+﻿
+
+using System;
+namespace DoNet.Code
+{
+    /// <summary>
+    /// DataOperateLogEntity
+    /// </summary>	
+    public class DataOperateLogEntity
+    {
+        public string F_Id { get; set; }
+        public string F_OperateType { get; set; }
+        public string F_DataId { get; set; } 
+        public string F_Account { get; set; }
+        public string F_NickName { get; set; }
+        public string F_IPAddress { get; set; }
+        public string F_IPAddressName { get; set; }
+        public string AssemblyName { get; set; }
+        public string ModelName { get; set; }
+        public string TableName { get; set; }
+        public string OldData { get; set; }
+        public string NewData { get; set; }
+        public DateTime F_CreatorTime { get; set; }
+        public string F_CreatorUserId { get; set; }
+        public long F_RowIndex { get; set; }
+    }
+
+    public class DataOperateLogContrastEntity
+    {
+        public string Key { get; set; }
+        public string Name { get; set; }
+        public string OldValue { get; set; }
+        public string NewValue { get; set; }
+    }
+}
---
+++ DoNet.Code/LogExt/LogType.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoNet.Code
+{
+    public enum DataOperateType
+    {
+        [Description("新增")]
+        Create = 1,
+        [Description("修改")]
+        Update = 2,
+        [Description("删除")]
+        Delete = 3
+    }
+}
